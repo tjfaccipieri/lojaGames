d781e0d5ec40480a7f8a46172a3c2985baa86f41
Thiago <thiago.faccipieri@gmail.com>
2021-10-24 19:31:49 -0300

    first deploy

--- src/app/listaprodutos/listaprodutos.component.ts
+++ src/app/listaprodutos/listaprodutos.component.ts
@@ -2,27 +2,51 @@ import { ProdutoService } from './../service/produto.service';
 import { Produto } from './../model/Produto';
 import { Component, OnInit } from '@angular/core';
 import { CategoriaService } from '../service/categoria.service';
+import { Categoria } from '../model/Categoria';
 
 @Component({
   selector: 'app-listaprodutos',
   templateUrl: './listaprodutos.component.html',
-  styleUrls: ['./listaprodutos.component.css']
+  styleUrls: ['./listaprodutos.component.css'],
 })
 export class ListaprodutosComponent implements OnInit {
-
   listaProdutos: Produto[] = [];
-  constructor(private prod: ProdutoService, private cat: CategoriaService) { }
+  listaCategorias: Categoria[] = [];
+  produto: Produto = new Produto();
+  constructor(
+    private prod: ProdutoService, 
+    private cat: CategoriaService
+    ) {}
 
   ngOnInit() {
     this.getProdutos();
+    this.getCategorias();
+    this.getProdutoById(4);
   }
 
-
   getProdutos() {
-    this.prod.getAllProdutos().subscribe(
-      (produtos: Produto[]) => {
-        this.listaProdutos = produtos;
-      }
-    );
+    this.prod.getAllProdutos().subscribe((produtos: Produto[]) => {
+      this.listaProdutos = produtos;
+    });
+  }
+
+  getCategorias() {
+    this.cat.getAllCategorias().subscribe((categorias: Categoria[]) => {
+      this.listaCategorias = categorias;
+    });
+  }
+
+  getProdutoById(id: number) {
+    this.prod.getProdutoById(id).subscribe((produto: Produto) => {
+      this.produto = produto;
+    });
+  }
+
+  editar() {}
+
+  deletarProduto() {
+    this.prod.deleteProduto(this.produto.id).subscribe(() => {
+      alert('Produto deletado com sucesso!');
+    });
   }
 }
