9d9d5f89a458f49cc850d50570be8b6c2a46cefe
Thiago <thiago.faccipieri@gmail.com>
2021-10-30 22:25:30 -0300

    finalizado login/cadastro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { Usuario } from './../model/Usuario';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
@@ -7,25 +8,74 @@ import { AuthService } from '../service/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  usuarioLogin: UsuarioLogin = new UsuarioLogin()
-  constructor(private auth: AuthService, private router: Router) { }
+  usuarioLogin: UsuarioLogin = new UsuarioLogin();
+  usuario: Usuario = new Usuario();
 
-  ngOnInit(){
+  confirmSenha: string;
+  constructor(private auth: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    // this.checkSenha();
   }
-  logar(){
+  logar() {
+    this.auth.logar(this.usuarioLogin).subscribe((resp: UsuarioLogin) => {
+      this.usuarioLogin = resp;
+      environment.nome = this.usuarioLogin.nome;
+      environment.usuario = this.usuarioLogin.usuario;
+      environment.token = this.usuarioLogin.token;
+      environment.id = this.usuarioLogin.id;
+      // console.log("🚀 ~ file: cadastro.component.ts ~ line 36 ~ CadastroComponent ~ this.auth.logar ~ environment", environment)
+      // alert('logou')
+      this.router.navigate(['/inicio']);
+    }, (error) => {
+      if (error.status === 401){
+        alert('Usuário ou senha inválido')
+      }
+    });
+  }
 
-    this.auth.logar(this.usuarioLogin).subscribe((resp: UsuarioLogin)=>{
-      this.usuarioLogin = resp
-      environment.nome = this.usuarioLogin.nome
-      environment.usuario = this.usuarioLogin.usuario
-      environment.token = this.usuarioLogin.token
-      environment.id = this.usuarioLogin.id
-      console.log("🚀 ~ file: cadastro.component.ts ~ line 36 ~ CadastroComponent ~ this.auth.logar ~ environment", environment)      
-      alert('logou')
-      this.router.navigate(['/inicio'])
-    })
+  confirmarSenha(event: any) {
+    this.confirmSenha = event.target.value;
+  }
+
+  cadastrar() {
+    this.usuario.tipo = 'cliente';
+    if (this.usuario.senha == this.confirmSenha) {
+      this.auth.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp;
+        alert('Usuário cadastrado com sucesso');
+        this.router.navigate(['/inicio']);
+      });
+    } else {
+      alert('As senhas não coincidem');
+    }
+  }
+
+  checkSenhaLogin() {
+    let inputSenha = document.getElementById('senhaLogin');
+    if (inputSenha?.getAttribute('type') == 'password') {
+      inputSenha.setAttribute('type', 'text');
+    } else {
+      inputSenha?.setAttribute('type', 'password');
+    }
+  }
+  checkSenhaCad() {
+    let inputSenha = document.getElementById('senha');
+    if (inputSenha?.getAttribute('type') == 'password') {
+      inputSenha.setAttribute('type', 'text');
+    } else {
+      inputSenha?.setAttribute('type', 'password');
+    }
+  }
+  checkConfirmSenha() {
+    let inputSenha = document.getElementById('confirmSenha');
+    if (inputSenha?.getAttribute('type') == 'password') {
+      inputSenha.setAttribute('type', 'text');
+    } else {
+      inputSenha?.setAttribute('type', 'password');
+    }
   }
 }
