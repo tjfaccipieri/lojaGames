e599989f8e3d30b773887b51a79046d0b5ebd866
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2021-11-09 10:31:40 -0300

    ajustado cadastro

--- src/app/model/Produto.ts
+++ src/app/model/Produto.ts
@@ -3,13 +3,16 @@ import { Categoria } from "./Categoria";
 
 export class Produto {
   public categoria: Categoria;
-  public tipo: string;
   public curtidos: number;
+  public dataLancamento: Date;
   public descricao: string;
+  public estilo: string;
   public foto: string;
   public id: number;
   public nome: string;
   public preco: number;
+  public publisher: string;
   public quantidade: number;
+  public tipo: string;
   public usuario: Usuario;
 }
